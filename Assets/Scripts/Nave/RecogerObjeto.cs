d62b0c165a8ef82f1d25afe25624417eee9adad5
Cristian Javier Arraya <arrayacristian75@gmail.com>
2025-10-31 00:31:39 -0300

    Levitacion de la oveja

---
+++ Assets/Scripts/Nave/RecogerObjeto.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RecogerObjeto : MonoBehaviour
+{
+    [SerializeField] Vector2 cuadradoTam = new Vector2(2f, 1f);
+    [SerializeField] float fuerza;
+    [SerializeField] LayerMask layerObjeto;
+
+    private void Update()
+    {
+        LevantarObjeto();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+    void LevantarObjeto() 
+    {
+        Vector2 boxCenter = (Vector2)transform.position + Vector2.down * (cuadradoTam.y / 2);
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(boxCenter, cuadradoTam, 0f, layerObjeto);
+        foreach (Collider2D col in colliders)
+        {
+            Rigidbody2D rb = col.attachedRigidbody;
+            if (rb != null)
+            {
+                rb.AddForce(Vector2.up * fuerza, ForceMode2D.Force);
+            }
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Vector2 boxcenter = (Vector2)transform.position + Vector2.down * (cuadradoTam.y / 2);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(boxcenter, cuadradoTam);
+    }
+}
