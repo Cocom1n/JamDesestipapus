d003c9d844210c18ca9f0f95dfde7b3383031c4f
Cristian Javier Arraya <arrayacristian75@gmail.com>
2025-11-01 19:47:57 -0300

    Se separo las acciones en script para mayor comodida

---
+++ Assets/Scripts/Interfaces/IHaceDanio.cs
@@ -0,0 +1,4 @@
+public interface IHaceDanio
+{
+    void HacerDanio();
+}
---
+++ Assets/Scripts/Jefe/Jefe.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Experimental.AI;
+
+public class Jefe : MonoBehaviour
+{
+   
+}
